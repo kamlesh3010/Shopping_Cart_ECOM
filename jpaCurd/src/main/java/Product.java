4166eb42c5e4862bc1320f053da0cb254b9880e7
Kamlesh Bobade <kamlesh928bobade@gmail.com>
2025-03-16 22:44:22 +0530

    Initial commit - Uploading Shopping Cart ECOM project

---
+++ jpaCurd/src/main/java/Product.java
@@ -0,0 +1,47 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    // Constructors, getters, and setters
+    public Product() {}
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
